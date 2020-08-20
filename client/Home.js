7f13d8f38b01fe99c6cf776194d2c5400db431a3
Daniel Sprague <danielaaronsprague@gmail.com>
2020-08-19 19:58:40 -0500

    now in DENIM.... lol

--- client/Home.js
+++ client/Home.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Home() {
   return (
     <div id='home'>
-      <h1>Welcome! To! DANIEL VS THE STACK!</h1>
-      <div id='stick-daniel'>
-        <img
-          className='title-item'
-          src='/danielvsthestack.svg'
-          alt='Daniel vs the stack'
-        ></img>
-      </div>
+      <h1>Welcome to!</h1>
+      <h2> DANIEL VS THE STACK! </h2>
+      <img
+        id='stick-daniel'
+        className='title-item'
+        src='/danielvsthestack.svg'
+        alt='Daniel vs the stack'
+      ></img>
       <p>
         Here lies the portfolio site of my engineering youth! As you can see
         things are still under construction around here but feel free to poke
