88149c34749e92fb4d18bf670515d37162f16e54
Daniel Sprague <danielaaronsprague@gmail.com>
2020-09-17 10:34:06 -0400

    new webpack

--- client/index.js
+++ client/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
